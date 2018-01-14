643e8482cd85c596062581b69f3047e82da3ac12
martinyounghoonkim <hoons0131@gmail.com>
2018-01-14 23:40:17 +0900

    Update unit test for App entry files

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,11 +1,24 @@
-import { mount } from 'avoriaz';
-
 import Vue from 'vue';
+import Vuex from 'vuex';
+
+import { mount } from 'avoriaz';
+import sinon from 'sinon';
+import axios from 'axios';
 import App from '@/App';
+import mutations from '@/store/mutations';
+
+Vue.use(Vuex);
 
 describe('App.vue', () => {
-  const AppComponent = mount(App);
-  it('Should render correct contents', () => {
-    expect(AppComponent.isVueComponent).to.equal(true);
-  });
+    let actions;
+    let store;
+
+    it('Should render correct contents', () => {
+        // const AppComponent = mount(App, { store });
+        // expect(AppComponent.isVueComponent).to.equal(true);
+    });
+
+    it('Should get correct data for todo app', () => {
+
+    })
 })
